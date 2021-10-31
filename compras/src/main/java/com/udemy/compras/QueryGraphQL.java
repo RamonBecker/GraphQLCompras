f412eedaeca604c43ac03603cf497cddbd755ce2
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-10-31 12:35:46 -0300

    Hello GraphQL & Type Query

---
+++ compras/src/main/java/com/udemy/compras/QueryGraphQL.java
@@ -0,0 +1,18 @@
+package com.udemy.compras;
+
+import org.springframework.stereotype.Component;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+@Component
+public class QueryGraphQL implements GraphQLQueryResolver {
+
+	public String hello() {
+		return "Hello graphQL";
+	}
+
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+}
