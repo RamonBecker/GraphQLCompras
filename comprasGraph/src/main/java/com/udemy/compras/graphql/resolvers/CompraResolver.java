a14c3cd0d8750acdee5bdbec385d3f2ca70ed231
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-11-01 17:13:57 -0300

    Resolver para os relacionamentos de Compra com Cliente/Produto

--- comprasGraph/src/main/java/com/udemy/compras/graphql/resolvers/CompraResolver.java
+++ comprasGraph/src/main/java/com/udemy/compras/graphql/resolvers/CompraResolver.java
@@ -1,12 +1,31 @@
 package com.udemy.compras.graphql.resolvers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.udemy.compras.graphql.entities.Cliente;
 import com.udemy.compras.graphql.entities.Compra;
+import com.udemy.compras.graphql.entities.Produto;
+import com.udemy.compras.graphql.service.ClienteService;
+import com.udemy.compras.graphql.service.ProdutoService;
 
 @Component
 public class CompraResolver implements GraphQLResolver<Compra> {
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	public Produto produto(Compra c) {
+		return produtoService.findById(c.getProduto().getId());
+	}
+
+	public Cliente cliente(Compra c) {
+		return clienteService.findById(c.getCliente().getId());
+	}
 
 	public String status(Compra c) {
 		return "Teste:" + c.getStatus();
